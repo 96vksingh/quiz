6138dd9beec061e9ae6f2cf92cf7ae406a5cfcf9
Vishal Kumar Singh <01vksingh@gmail.com>
2019-07-19 10:50:42 +0530

    subjects added

--- app/src/main/java/com/example/quiz/maths_q.java
+++ app/src/main/java/com/example/quiz/maths_q.java
@@ -2,13 +2,26 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class maths_q extends AppCompatActivity {
+    private Button kk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maths_q);
+        kk=findViewById(R.id.button2);
+        kk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent fr = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(fr);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quiz/science_q.java
+++ app/src/main/java/com/example/quiz/science_q.java
@@ -2,13 +2,25 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class science_q extends AppCompatActivity {
+    private Button bb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science_q);
+        bb= findViewById(R.id.button);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent back = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(back);
+            }
+        });
     }
 }
