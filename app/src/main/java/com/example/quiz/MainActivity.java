24b896ad0028f92cd4aa2cb477fec7e6bfa37329
Vishal Kumar Singh <01vksingh@gmail.com>
2019-07-19 10:20:42 +0530

    subjects added

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -2,36 +2,49 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioGroup subjects;
 
-    private RadioButton m,s,e,c;
+
+    private RadioButton ss,m,s,e,c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        m = findViewById(R.id.radioButton);
-        s = findViewById(R.id.radioButton2);
+        subjects = findViewById(R.id.subs);
+
+        m = findViewById(R.id.radioButton4);
+        s = findViewById(R.id.radioButton);
         e = findViewById(R.id.radioButton3);
-        c = findViewById(R.id.radioButton4);
+        c = findViewById(R.id.radioButton2);
 
         m.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
                 Toast.makeText(getApplicationContext(),"MATHS",Toast.LENGTH_SHORT).show();
+
+                Intent mm = new Intent(getBaseContext(),maths_q.class);
+                startActivity(mm);
             }
         });
         s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"science",Toast.LENGTH_SHORT).show();
+                Intent ss = new Intent(getBaseContext(),science_q.class);
+                startActivity(ss);
             }
         });
 
@@ -49,5 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 }
