b2ed688efa3ae2d69bd69615a0c8b4ff41f89142
Vishal Kumar Singh <01vksingh@gmail.com>
2019-07-19 09:22:20 +0530

    subjects added

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioButton m,s,e,c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        m = findViewById(R.id.radioButton);
+        s = findViewById(R.id.radioButton2);
+        e = findViewById(R.id.radioButton3);
+        c = findViewById(R.id.radioButton4);
+
+        m.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"MATHS",Toast.LENGTH_SHORT).show();
+            }
+        });
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"science",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        e.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"ENGLISH",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"computers",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
