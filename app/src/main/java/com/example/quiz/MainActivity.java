dcf98bed5cfaa695fc4cd195fa13dd3e0d32f26e
Vishal Kumar Singh <01vksingh@gmail.com>
2019-08-01 11:55:00 +0530

    menu added

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -15,11 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
 
     private RadioButton ss,m,s,e,c;
+    private Button hg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hg = findViewById(R.id.button3);
+
 
         subjects = findViewById(R.id.subs);
 
@@ -35,16 +42,14 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast.makeText(getApplicationContext(),"MATHS",Toast.LENGTH_SHORT).show();
 
-                Intent mm = new Intent(getBaseContext(),maths_q.class);
-                startActivity(mm);
+
             }
         });
         s.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"science",Toast.LENGTH_SHORT).show();
-                Intent ss = new Intent(getBaseContext(),science_q.class);
-                startActivity(ss);
+
             }
         });
 
@@ -63,5 +68,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu mm){
+        getMenuInflater().inflate(R.menu.items,mm);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem ii){
+        //int id = ii.getItemId();
+        switch (ii.getItemId()){
+            case R.id.i1 :
+                Toast.makeText(getApplicationContext(),"cccc item 1 was selected",Toast.LENGTH_SHORT).show();
+                Intent mm = new Intent(getBaseContext(),maths_q.class);
+                startActivity(mm);
+
+                break;
+            case R.id.i2 :
+                Toast.makeText(getApplicationContext(),"cccc item 2 was selected",Toast.LENGTH_SHORT).show();
+                Intent ss = new Intent(getBaseContext(),science_q.class);
+                startActivity(ss);
+                break;
+            case R.id.i3 :
+                Toast.makeText(getApplicationContext(),"cccc item 3 selected",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.i4 :
+                Toast.makeText(getApplicationContext(),"ccccc item 4 selected",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(ii);
     }
 }
